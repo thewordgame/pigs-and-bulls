73a006dac7d8878b0d51b7cd7d52c33fec46b64e
deshondixon <deshondixon@gmail.com>
2022-10-26 11:59:54 -0700

    updatedagain

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -1,1 +1,53 @@
 // where the chart logic goes
+
+function renderChart() {
+  const parseWord = [];
+  const insertHere  = [];
+  const insertHere  = [];
+  
+  for (let i=0; i < parseWord.length; i += 1) {
+    parseWord.push(insertHere[i].insertHere);
+    insertHere.push(insertHere[i].insertHere);
+    insertHere.push(insertHere[i].insertHere);
+  }
+  let data = {
+    labels: pigsAndBulls,
+    datasets: [{
+      label: '# of insertHere',
+      data: pigsAndBulls,
+      backgroundColor: [
+        'red',
+      ],
+      borderColor: [
+        'blue',
+      ],
+      borderWidth: 10,
+    }, {
+      label: '# of insertHere',
+      data: insertHere,
+      backgroundColor: [
+        'lime',
+      ],
+      borderColor: [
+        'orange',
+      ],
+      borderWidth: 12,
+    }],
+  };
+  const config = {
+    type: 'bar',
+    data: insertHere,
+    options: {
+      indexAxis: 'y',
+      scales: {
+        y: {
+          stacked: true,
+        },
+      },
+    },
+  };
+  const myChart = new Chart(
+    document.getElementById('myChart'),
+    config,
+  );
+}
